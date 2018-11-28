38520557746ec2146376bc8b2feb58e68ee8e7dc
Zita Balint <zitag4@gmail.com>
2018-11-28 17:45:51 +0100

    import Link

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,10 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Search extends Component {
 
 }
 
-export default Search;
+
+
+export default Search
