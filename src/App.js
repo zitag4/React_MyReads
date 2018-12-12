417439d0cd75ac81da0b4c27af7658c498751c77
Zita Balint <zitag4@gmail.com>
2018-12-12 14:37:20 +0100

    Add "books" array to component state

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import { Route, Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-const books=[{'id':'1', 'name': 'en'},
-              {'id':'2', 'name': 'te'},
-              {'id':'3', 'name': 'o'}];
-
 class BooksApp extends React.Component {
+  //Add array to component state
+  state = {
+    books: [{'id':'1', 'name': 'en'},
+                  {'id':'2', 'name': 'te'},
+                  {'id':'3', 'name': 'o'}]
+  }
 
   render() {
-
       return (
         <div>
-          <ListBooks books={books}/>
+          <ListBooks books={this.state.books}/>
 
           <Route
             exact path='/search'
