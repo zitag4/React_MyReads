ed11ea7ab316f4b76f83384cd88778948120997e
Zita Balint <zitag4@gmail.com>
2018-12-17 16:07:13 +0100

    Implement Book component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import escapeRegExp from 'escape-string-regexp'
-import sortBy from 'sort-by'
+import Book from './Book'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 
@@ -118,29 +117,10 @@ class ListBooks extends Component{
         {this.state.showingBooks.map( (book) => (
           <li key={book.id} className='list-book-item'>
           {/*Creating the list items (books)*/}
-          <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-              <div className="book-shelf-changer">
-                <select>
-                  <option value="move" disabled>Move to...</option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-                {/*Invoke onDeleteBook when the button is clicked on
-                //iterating over whit specific book which is comming from .mop function*/}
-                <button onClick={ () => onDeleteBook(book) } className='book-remove'>
-                  Remove
-                </button>
-              </div>
-            </div>
-            <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authors}</div>
-          </div>
-
-
+              <Book
+                authors={ book.authors }
+                title={ book.title }
+              />
           </li>
         ))}
       </ol>
